29ae963945d8df8e9b5015fac6bf64faefb871f1
Sami <samiunn@hotmail.com>
2015-03-14 03:48:40 +0700

    Init commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+var b = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: [-5, 2, 5, -2]});
+var p1 = b.create('point',[-1,1], {name:'A',size:4});
+var p2 = b.create('point',[2,-1], {name:'B',size:4});
